dfb3809c9cb8829aa42a8f6ad5aa3bb2fe7df534
Colin Davis <nilocsivad@aol.com>
2016-10-17 11:02:19 +0800

    Send Html Mail

--- src/main/java/com/iamVip/mail2/request/req/ReqMailRequest.java
+++ src/main/java/com/iamVip/mail2/request/req/ReqMailRequest.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.iamVip.mail2.rs.util.DTUtil;
 import com.iamVip.mail2.rs.util.MailSender;
 import com.iamVip.mail2.rs.util.MapUtil;
 
@@ -35,8 +36,18 @@ public class ReqMailRequest extends __ReqRequest {
 	@ResponseBody
 	@RequestMapping(value = { "send/{subject}/{text}" })
 	public Map<String, Object> mailNoLimit(String to, @PathVariable String subject, @PathVariable String text, HttpServletRequest request) throws Exception {
-		mailSender.sendTextMail(subject, text, new String[] { to });
+		mailSender.sendTextMail(subject, new String[] { to }, text);
 		return MapUtil.mapThem(new String[] { "to", "subject", "text" }, to, subject, text);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = { "send/html/{subject}" })
+	public Map<String, Object> mailWithHtmlNoLimit(String to, @PathVariable String subject, HttpServletRequest request) throws Exception {
+
+		Map<String, Object> map = MapUtil.mapThem(new String[] { "to", "subject", "timeMillis", "datetime" }, to, subject, System.currentTimeMillis(), DTUtil.nowDT());
+		mailSender.sendHtmlMail(subject, new String[] { to }, map);
+
+		return map;
 	}
 
 }
--- src/main/java/com/iamVip/mail2/rs/util/MailSender.java
+++ src/main/java/com/iamVip/mail2/rs/util/MailSender.java
@@ -1,33 +1,76 @@
 package com.iamVip.mail2.rs.util;
 
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 /**
  * @author Colin
  */
 public class MailSender extends JavaMailSenderImpl {
 
-	private static String[] USERS, PWDS;
+	@Autowired
+	private VelocityEngine velocityEngine;
 
-	public MailSender(String[] users, String[] pwds) {
-		USERS = users;
-		PWDS = pwds;
+	private String[] users, pwds, templates;
+
+	public MailSender(String[] users, String[] pwds, String[] templates) {
+		this.users = users;
+		this.pwds = pwds;
+		this.templates = templates;
 	}
 
-	public void sendTextMail(String subject, String text, String[] to) {
+	public void sendTextMail(String subject, String[] to, String text) {
 
-		int index = (int) System.currentTimeMillis() % USERS.length;
+		int index = (int) System.currentTimeMillis() % users.length;
 
 		SimpleMailMessage mail = new SimpleMailMessage();
 		mail.setTo(to);
-		mail.setFrom(USERS[index]);
+		mail.setFrom(users[index]);
 		mail.setSubject(subject + "-" + DTUtil.nowTime());
 		mail.setText(text + " --- " + DTUtil.nowDT());
 
-		this.setUsername(USERS[index]);
-		this.setPassword(PWDS[index]);
+		this.setUsername(users[index]);
+		this.setPassword(pwds[index]);
+		this.send(mail);
 
+	}
+
+	public void sendHtmlMail(String subject, String[] to, Map<String, Object> map) {
+		this.sendHtmlMail(subject, to, 0, map);
+	}
+
+	/**
+	 * @param whichOne 0:注册验证码 1:找回密码验证码 ...其它待定
+	 */
+	public void sendHtmlMail(String subject, String[] to, int whichOne, Map<String, Object> map) {
+
+		int index = (int) System.currentTimeMillis() % users.length;
+
+		MimeMessagePreparator mail = new MimeMessagePreparator() {
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				
+				String body = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, templates[whichOne], "UTF-8", map);
+				
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+				message.setTo(to);
+				message.setFrom(users[index]);
+				message.setSubject(subject);
+				message.setText(body, true);
+			}
+		};
+
+		this.setUsername(users[index]);
+		this.setPassword(pwds[index]);
 		this.send(mail);
 
 	}
